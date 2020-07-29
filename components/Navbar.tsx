0837415933c3ebca5bf8a06f7fc528c262f3feed
maxikgreat <maximvasylenko228322@gmail.com>
2020-07-29 20:35:28 +0200

    Linking and getServerSideProps

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,25 +1,25 @@
+import Link from 'next/link';
+
 export const Navbar = () => (
   <nav className="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
     <div className="container">
-      <a className="navbar-brand" href="#">Start Bootstrap</a>
+      <Link href="/"><a className="navbar-brand" href="#">Start Bootstrap</a></Link>
       <button className="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation">
         <span className="navbar-toggler-icon"></span>
       </button>
       <div className="collapse navbar-collapse" id="navbarResponsive">
         <ul className="navbar-nav ml-auto">
           <li className="nav-item active">
-            <a className="nav-link" href="#">Home
-              <span className="sr-only">(current)</span>
-            </a>
+            <Link href="/">
+              <a className="nav-link">Home
+                <span className="sr-only">(current)</span>
+              </a>
+            </Link>
           </li>
           <li className="nav-item">
-            <a className="nav-link" href="#">About</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Services</a>
-          </li>
-          <li className="nav-item">
-            <a className="nav-link" href="#">Contact</a>
+            <Link href="/about">
+              <a className="nav-link">About</a>
+            </Link>
           </li>
         </ul>
       </div>
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,40 +1,44 @@
-import Head from 'next/head'
+import { useState, useEffect } from 'react';
+import Head from 'next/head';
 import { Navbar } from '../components/Navbar';
 import { SideMenu } from '../components/SideMenu';
 import { Carousel } from '../components/Carousel';
 import { MoveList } from '../components/MoveList';
 import { Footer } from '../components/Footer';
+import { getMovies } from '../actions/index';
+import { Movie } from '../types';
 
-export default function Home() {
-  const MOVIE_DATA = [
-    {
-      id: '1',
-      name: 'The Shawshank Redemption',
-      releaseYear: 1994,
-      description: 'Two imprisoned men bond over a number of years, finding solace and eventual redemption through acts of common decency.',
-      rating: 4.8,
-      genre: 'drama',
-      image: 'https://m.media-amazon.com/images/M/MV5BNjQ2NDA3MDcxMF5BMl5BanBnXkFtZTgwMjE5NTU0NzE@._V1_CR0,60,640,360_AL_UX477_CR0,0,477,268_AL_.jpg'
-    },
-    {
-      id: '2',
-      name: 'The Dark Knight',
-      releaseYear: 2008,
-      description: 'When the menace known as The Joker emerges from his mysterious past, he wreaks havoc and chaos on the people of Gotham. The Dark Knight must accept one of the greatest psychological and physical tests of his ability to fight injustice.',
-      rating: 4.7,
-      genre: 'action, crime, drama',
-      image: 'https://img.cinemablend.com/filter:scale/quill/c/3/8/0/f/4/c380f4f12cfeec19f0c40c6f57db188f2f98cca8.jpg?mw=600'
-    },
-    {
-      id: '3',
-      name: 'Lord of the Rings',
-      releaseYear: 2004,
-      description: 'A meek Hobbit from the Shire and eight companions set out on a journey to destroy the powerful One Ring and save Middle-earth from the Dark Lord Sauron.',
-      rating: 4.9,
-      genre: 'adventure, drama, fantasy',
-      image: 'https://img.cinemablend.com/filter:scale/quill/0/f/5/2/a/6/0f52a6843a25c1a5c1f9a0c00548cad9e1d912e2.jpg?mw=600'
-    }	  
-  ];
+interface MovieState {
+  data: Movie[] | [],
+  error: string,
+}
+interface HomeProps {
+  movies: Movie[] | [],
+}
+
+export async function getServerSideProps() {
+  const movies = await getMovies();
+
+  return { props: { movies } }
+}
+
+export default function Home({ movies }: HomeProps) {
+  // const [movies, setMovies] = useState<MovieState>({
+  //   data: [],
+  //   error: '',
+  // });
+
+  // useEffect(() => {
+  //   const fetchMovies = async () => {
+  //     try {
+  //       const resMovies = await getMovies();
+  //       setMovies({ ...movies, data: resMovies });
+  //     } catch (error) {
+  //       setMovies({ ...movies, error });
+  //     }
+  //   };
+  //   fetchMovies();
+  // }, []);
 
   return (
     <div>
@@ -56,7 +60,7 @@ export default function Home() {
               <Carousel />
               <div className="row">
                 <MoveList
-                  movies={MOVIE_DATA}
+                  movies={movies}
                 />
               </div>
             </div>
